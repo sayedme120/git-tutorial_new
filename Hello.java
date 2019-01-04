48c11b70a9bb201b2068db0493de849dc2941619
sayedme120 <sayedme120@gmail.com>
2019-01-04 16:53:04 +0600

    Keep both method

--- Hello.java
+++ Hello.java
@@ -2,11 +2,15 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vs Code is Awesome....");
 
-		greeting("Git");
+		display();
 	}
 
 	public static void greeting(String[] name) {
-		System.out.println("Bye Bye" + name);
+		System.out.println("welcome" + name);
+	}
+
+	public static void display() {
+		System.out.println("Hello Hello");
 	}
 
 	public static int sqr(int x){
