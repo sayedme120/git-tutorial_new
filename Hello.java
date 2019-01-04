c00dce65c0b39728784b4271e06e90a2e507445b
sayedme120 <sayedme120@gmail.com>
2019-01-04 17:06:50 +0600

    all method removed add message method

--- Hello.java
+++ Hello.java
@@ -2,19 +2,11 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vs Code is Awesome....");
 
-		display();
 	}
 
-	public static void greeting(String[] name) {
-		System.out.println("welcome" + name);
+	public static void message(){
+		System.out.println("Hooray!! Now we are able to contribute on open source....");
 	}
 
-	public static void display() {
-		System.out.println("Hello Hello");
-	}
-
-	public static int sqr(int x){
-		return x*x;
-	}
 
 }
