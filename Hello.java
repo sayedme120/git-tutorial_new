8a7b90a27a26ee8d432a26c828297c998bd39275
sayedme120 <sayedme120@gmail.com>
2019-01-04 15:22:40 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vs Code is Awesome....");
+
+		greeting("ProSayed");
 	}
+
+	public static void greeting(String[] name) {
+		System.out.println("welcome" + name);
+	}
+
 }
